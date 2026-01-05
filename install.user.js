c9bfce8c2b280ccf58f2220aefdc19743e9127f1
strlisc <minhkhangcaohuynh1234@gmail.com>
2026-01-05 18:46:40 +0700

    Update metadata in install.user.js

--- install.user.js
+++ install.user.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name         DuoHacker Full Version
 // @description  Best Free Duolingo Hack with XP Farming, Gems Farming, Streaks Farming, Auto Quest Daily!
-// @namespace    https://twisk.fun
+// @namespace    https://twisk.fun/install
 // @version      1.1.0
-// @author       DuoHacker Community
+// @author       MintDevs
 // @match        https://*.duolingo.com/*
 // @match        https://*.duolingo.cn/*
 // @icon         https://github.com/helloticc/DuoHacker/blob/main/DuoHacker.png?raw=true
-// @license      MIT
-// @require 
+// @require https://raw.githubusercontent.com/helloticc/DuoHacker/refs/heads/main/script.js
 // ==/UserScript==
