55f08b38d4b132a62235557c7a0b9ae6190b6993
Future <minhkhangcaohuynh1234@gmail.com>
2026-01-17 18:41:23 +0700

    Update install.user.js

--- install.user.js
+++ install.user.js
@@ -9,5 +9,8 @@
 // @icon         https://github.com/FutureCLI/DuoHacker/blob/main/Logo_TypePNG_DuoHacker.png?raw=true
 // @grant        none
 // @license      MIT
-// @require https://raw.githubusercontent.com/DuoHacker/DuoHacker-Project/refs/heads/main/Sources/main.js
+// @require https://raw.githubusercontent.com/FutureCLI/DuoHacker/refs/heads/main/sources/main.js
 // ==/UserScript==
+
+/* This is an obfuscated script, so if you don't want to use, you can use the Lite version (100% open-source) here -> https://greasyfork.org/en/scripts/561041-duolingo-duohacker
+or you can use direct link : https://update.greasyfork.org/scripts/561041/Duolingo%20DuoHacker.user.js
